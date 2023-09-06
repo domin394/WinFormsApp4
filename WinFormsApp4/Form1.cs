c390f1fb241699750bfd17ff3db54a4debc387ed
Dominik Littwin <dominik.littwin@teb.edu.pl>
2023-09-06 12:39:35 +0200

    zmiany kierunku przycisku

--- WinFormsApp4/Form1.cs
+++ WinFormsApp4/Form1.cs
@@ -9,7 +9,30 @@ namespace WinFormsApp4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
+
+            //szerokoœæ okna aplikacji
+            int windowWidth = Size.Width;
+            //wysokoœæ okna aplikacji
+            int windowHeight = Size.Height;
+
+            windowWidth -= button1.Width;
+            windowHeight -= button1.Height;
+
+            //inicjalizacja generatora liczb losowych
+            Random rnd = new Random();
+
+            //losowa odleg³oœæ od lewej - nie wiêcej ni¿ szerokoœæ okna
+            int randomLeft = rnd.Next(windowWidth);
+
+            //analogicznie z odleg³oœci¹ od góry
+            int randomTop = rnd.Next(windowHeight);
+
+            //przesuwamy guzik
+            //location musi byæ przekazywany jako punkt na ekranie
+            Point topLeftCorner = new Point(randomLeft, randomTop);
+            //centerButton to nazwa (atrybut Name) mojego guzika
+            button1.Location = topLeftCorner;
         }
     }
 }
